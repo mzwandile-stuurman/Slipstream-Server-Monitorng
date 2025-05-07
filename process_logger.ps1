9f7f19cda34fdcb1c5a72c5768ee0aa4337c77d7
Mzwandile Stuurman <82204806+mzwandile-stuurman@users.noreply.github.com>
2025-05-07 17:09:21 +0200

    final project poc changes

--- process_logger.ps1
+++ process_logger.ps1
@@ -5,42 +5,62 @@ $connectionString = "DSN=PostgreSQL_DNS;UID=postgres;PWD=mzwandile;"
 $connection = New-Object System.Data.Odbc.OdbcConnection($connectionString)
 $connection.Open()
 
-# System Info
-$cpuCount = (Get-CimInstance -ClassName Win32_ComputerSystem).NumberOfLogicalProcessors
-$totalRAM = (Get-CimInstance -ClassName Win32_ComputerSystem).TotalPhysicalMemory
+# Excluding background data
+$excludedProcesses = @(
+    "Idle", "System", "svchost", "wininit", "csrss", "services", "lsass", "smss",
+    "winlogon", "conhost", "fontdrvhost", "WUDFHost", "WmiPrvSE", "dllhost"
+)
+
+# Timestamp
 $timestamp = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
 $logDate = (Get-Date).ToString("yyyy-MM-dd")
 $machine = $env:COMPUTERNAME
 
-# Network stats : still searching for a better way to collect this data
-$netSent = (Get-Counter '\Network Interface(*)\Bytes Sent/sec').CounterSamples | Measure-Object -Property CookedValue -Sum
-$netRecv = (Get-Counter '\Network Interface(*)\Bytes Received/sec').CounterSamples | Measure-Object -Property CookedValue -Sum
+# CPU INFO
+$cpuInfo = Get-CimInstance -ClassName Win32_Processor
+$cpuName = $cpuInfo.Name
+$cpuLoad = $cpuInfo.LoadPercentage
+$cpuCores = $cpuInfo.NumberOfLogicalProcessors
+$maxClock = $cpuInfo.MaxClockSpeed
+$currentClock = $cpuInfo.CurrentClockSpeed
 
-$bytesSent = [math]::Round($netSent.Sum)
-$bytesRecv = [math]::Round($netRecv.Sum)
-
-# Memory stats : can be done on tableau are null 
+# MEMORY
 $os = Get-CimInstance -ClassName Win32_OperatingSystem
 $totalMem = [math]::Round($os.TotalVisibleMemorySize / 1024, 2)   # in MB
 $freeMem = [math]::Round($os.FreePhysicalMemory / 1024, 2)
 $usedMem = [math]::Round($totalMem - $freeMem, 2)
 $memPercentUsed = [math]::Round(($usedMem / $totalMem) * 100, 2)
 
-# Insert System KPIs
+# NETWORK
+$netSent = (Get-Counter '\Network Interface(*)\Bytes Sent/sec').CounterSamples | Measure-Object -Property CookedValue -Sum
+$netRecv = (Get-Counter '\Network Interface(*)\Bytes Received/sec').CounterSamples | Measure-Object -Property CookedValue -Sum
+$bytesSent = [math]::Round($netSent.Sum)
+$bytesRecv = [math]::Round($netRecv.Sum)
+
+# INSERT SYSTEM METRICS
 $sysCmd = $connection.CreateCommand()
 $sysCmd.CommandText = @"
 INSERT INTO system_metrics (
-    timestamp, log_date, cpu_count, total_physical_memory, total_net_sent, total_net_received,
-    total_memory_mb, free_memory_mb, used_memory_mb, ram_percent_used, machine_name
+    timestamp, log_date, cpu_count, total_physical_memory,
+    total_net_sent, total_net_received,
+    total_memory_mb, free_memory_mb, used_memory_mb, ram_percent_used,
+    machine_name, cpu_name, cpu_load, max_clock_mhz, current_clock_mhz
 ) VALUES (
-    '$timestamp', '$logDate', $cpuCount, $totalRAM, $bytesSent, $bytesRecv,
-    $totalMem, $freeMem, $usedMem, $memPercentUsed, '$machine'
+    '$timestamp', '$logDate', $cpuCores, $($os.TotalVisibleMemorySize * 1024),
+    $bytesSent, $bytesRecv,
+    $totalMem, $freeMem, $usedMem, $memPercentUsed,
+    '$machine', '$cpuName', $cpuLoad, $maxClock, $currentClock
 )
 "@
 $sysCmd.ExecuteNonQuery() | Out-Null
 
-# Per-process collection : will be refined later
-$processes = Get-Process
+# COLLECT PROCESS METRICS
+$processes = Get-Process | Where-Object {
+    $_.Name -notin $excludedProcesses -and
+    $_.CPU -gt 0 -and
+    $_.WorkingSet -gt 1MB
+}
+
 foreach ($p in $processes) {
     try {
         $cmd = $connection.CreateCommand()
@@ -74,19 +94,30 @@ foreach ($p in $processes) {
         $otherBytes = if ($p.IO) { $p.IO.OtherTransferCount } else { 0 }
         $totalDiskIO = $readBytes + $writeBytes + $otherBytes
 
+        # CPU % estimate
+        $uptime = (Get-Date) - $p.StartTime
+        $uptimeSecs = $uptime.TotalSeconds
+        $cpuPercent = if ($uptimeSecs -gt 0) {
+            [math]::Round(($totalSecs / $uptimeSecs) * 100, 2)
+        } else {
+            0
+        }
+
         $cmd.CommandText = @"
 INSERT INTO process_monitor (
     name, session_id, ws, vm, private_mem, working_set,
     virtual_mem, paged_mem, peak_ws, peak_vm, max_ws, cpu,
     priv_proc_time, user_proc_time, total_proc_time, processor_affinity,
     start_time, has_exited, responding, machine_name,
-    read_bytes, write_bytes, other_bytes, total_disk_io, timestamp, log_date
+    read_bytes, write_bytes, other_bytes, total_disk_io,
+    cpu_percent, timestamp, log_date
 ) VALUES (
     '$escapedName', $si, $ws, $vm, $privMem, $workingSet,
     $virtMem, $pagedMem, $peakWS, $peakVM, $maxWS, $cpu,
     $privSecs, $userSecs, $totalSecs, '$affinity',
     '$startTime', '$hasExited', '$responding', '$machine',
-    $readBytes, $writeBytes, $otherBytes, $totalDiskIO, '$timestamp', '$logDate'
+    $readBytes, $writeBytes, $otherBytes, $totalDiskIO,
+    $cpuPercent, '$timestamp', '$logDate'
 )
 "@
         $cmd.ExecuteNonQuery() | Out-Null
